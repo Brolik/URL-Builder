77fb1117e453617d5363b043fb61e823a3872c01
Kyle Jasso <jassok@brolik.com>
2014-03-20 10:17:14 -0400

    all things populating

--- js/app.js
+++ js/app.js
@@ -15,9 +15,7 @@ builder.config(['$routeProvider', function($routeProvider) {
         });
 }]);
 
-builder.controller('mainCtrl',
-    ['$scope','$http','$sce',
-    function($scope,$http, $sce) {
+builder.controller('mainCtrl', ['$scope','$http','$sce', function($scope,$http, $sce) {
 
     // Handle the popup.
     $scope.modalShow = false;
@@ -34,7 +32,7 @@ builder.controller('mainCtrl',
 
                 for(var i = 0; i < response.length; i++) {
                     if(response[i].ID === content) {
-                        $scope.dialogTitle = $sce.trustAdHtml(response[i].title);
+                        $scope.dialogTitle = $sce.trustAsHtml(response[i].title);
                         $scope.dialogContent = $sce.trustAsHtml(response[i].content);
                     }
                 }
@@ -51,7 +49,10 @@ builder.controller('mainCtrl',
     $scope.fields = [
         { type: 'input' , title: 'Campaign URL', required: true, model: 'url', custom: false },
         { type: 'select', title: 'Campaign Source', required: true, model: 'source', custom: true },
-        { type: 'select', title: 'Campaign Medium', required: true, model: 'medium', custom: true }
+        { type: 'select', title: 'Campaign Medium', required: true, model: 'medium', custom: true },
+        { type: 'input',  title: 'Campaign Name' , required: true, model: 'campaign', cusom: false },
+        { type: 'input',  title: 'Campaign Term', required: false, model: 'term', custom: false },
+        { type: 'input',  title: 'Campaign Content', required: false, model: 'content', custom: false }
     ];
 
     $scope.sources = [
@@ -81,32 +82,3 @@ builder.controller('mainCtrl',
 builder.controller('helpCtrl', ['$scope', function($scope) {
 
 }]);
-
-/*
-
-$scope.fields = [
-    { type: 'input' , title: 'Campaign URL', required: true, help: 'url', custom: false}
-]
-
-$scope.sources = [
-    { value:'newsletter',       title:'Newsletter'},
-    { value:'facebook.com',     title:'Facebook'  },
-    { value:'google',           title:'google'    },
-    { value:'bing',             title:'bing'      },
-    { value:'twitter.com',      title:'twitter'   },
-    { value:'yahoo',            title:'yahoo'     },
-    { value:'youtube.com',      title:'youtube'   },
-    { value:'linkedin.com',     title:'linkedin'  },
-    { value:'pinterest.com',    title:'pinterest' }
-];
-
-$scope.mediums = [
-    { value:'cpc',      title:'CPC'      },
-    { value:'cpm',      title:'CPM'      },
-    { value:'cpa',      title:'CPA'      },
-    { value:'referral', title:'Referral' },
-    { value:'organic',  title:'organic'  },
-    { value:'email',    title:'email'    }
-];
-
-*/
--- js/directives.js
+++ js/directives.js
@@ -6,9 +6,6 @@ builder.directive('modalDialog', function() {
     },
     replace: true, // Replace with the template below
     transclude: true, // we want to insert custom content inside the directive
-    compile: function(element, attrs) {
-        console.log(element);
-    },
     link: function(scope, element, attrs) {
         scope.hideModal = function() {
             scope.show = false;
